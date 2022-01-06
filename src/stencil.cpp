009c6f246fe6c4a51412e66dec6c5a11cbe9b249
Fatbardh <fatbardh.kadriu@student.uni-pr.edu>
2022-01-06 18:32:41 +0100

    implement parallel execution of matrix

--- src/stencil.cpp
+++ src/stencil.cpp
@@ -2,36 +2,71 @@
 #include <iostream>
 #include <cmath>
 
-constexpr auto N = 4;
+constexpr auto N = 20;
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-
-    float square_matrix[N][N];
+int main(int argc, char *argv[])
+{
+    float sequential_matrix[N][N];
+    float parallel_matrix[N][N];
     for (int i = 0; i < N; i++)
     {
-        square_matrix[0][i] = 250;
-        square_matrix[i][0] = 150;
+        sequential_matrix[0][i] = 250;
+        sequential_matrix[i][0] = 150;
+        parallel_matrix[0][i] = 250;
+        parallel_matrix[i][0] = 150;
     }
-	square_matrix[0][0] = 0;
+    sequential_matrix[0][0] = 0;
+    parallel_matrix[0][0] = 0;
 
     for (int i = 1; i < N; i++)
     {
         for (int j = 1; j < N; j++)
         {
-            square_matrix[i][j] = (fabs(sin(square_matrix[i - 1][j - 1])) +
-                fabs(sin(square_matrix[i][j - 1])) +
-                fabs(sin(square_matrix[i - 1][j]))) * 100;
+            sequential_matrix[i][j] = (fabs(sin(sequential_matrix[i - 1][j - 1])) +
+                                       fabs(sin(sequential_matrix[i][j - 1])) +
+                                       fabs(sin(sequential_matrix[i - 1][j]))) *
+                                      100;
         }
     }
 
-    omp_set_num_threads(3);
-#pragma omp parallel
+    for (int i = 2; i < N * 2; i++)
     {
-    
-        cout << "Pershendetje ";
-    
+#pragma omp parallel for num_threads(4)
+            for (int j = 1; j < i; j++)
+            {
+                int row = i - j;
+                if (row < N && j < N)
+                {
+                    parallel_matrix[row][j] = (fabs(sin(parallel_matrix[row][j - 1])) +
+                                               fabs(sin(parallel_matrix[row - 1][j - 1])) +
+                                               fabs(sin(parallel_matrix[row - 1][j]))) *
+                                              100;
+                }
+            }
     }
-	return 0;
+
+    bool solutionValid = true;
+    for (int k = 0; k < N; k++)
+    {
+        for (int l = 0; l < N; l++)
+        {
+            if (sequential_matrix[k][l] != parallel_matrix[k][l])
+            {
+                solutionValid = false;
+            }
+        }
+    }
+
+    if (solutionValid)
+    {
+        cout << "Solution is valid." << endl;
+    }
+    else
+    {
+        cout << "Wrong!! Matrices are not equal." << endl;
+    }
+
+    return 0;
 }
