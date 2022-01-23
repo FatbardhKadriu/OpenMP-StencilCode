201edbae409c4defeb77441698f9e6d268f5eb0f
Arbena Musa <43915605+ArbenaMusa@users.noreply.github.com>
2022-01-23 12:29:20 +0100

    Tests for different no. of threads and matrix dimensions

--- src/stencil.cpp
+++ src/stencil.cpp
@@ -16,57 +16,63 @@
 
 using namespace std;
 
-/* defines the number of matrix rows */
-constexpr auto NR_ROWS = 46080;
-/* defines the number of matrix cols */
-constexpr auto NR_COLS = 46080;
 /* defines the number of threads for parallel execution */
-constexpr auto THREADS = 8;
+int threads[5]    = { 2, 4, 8, 16, 32 };
+/* defines the matrix dimensions */
+int dimensions[8] = { 360, 720, 1440, 2880, 5760, 11520, 23040, 46080 };
 
 float **sequential_matrix;
 float **parallel_matrix  ;
 
-void initialize_matrices ();
-void execute_sequential  ();
-void execute_parallel    ();
-bool isSolutionCorrect   ();
+void initialize_matrices (int nr_rows, int nr_cols);
+void execute_sequential  (int nr_rows, int nr_cols);
+void execute_parallel    (int nr_rows, int nr_cols, int threads);
+bool isSolutionCorrect   (int nr_rows, int nr_cols);
 
 int main(int argc, char *argv[])
 {
-    initialize_matrices();
-    
-    auto start_time_sequential = chrono::high_resolution_clock::now();
-    execute_sequential();
-    auto end_time_sequential = chrono::high_resolution_clock::now();
-
-    float elapsed_time_sequential = chrono::duration<float, std::milli>
-                                            (end_time_sequential - start_time_sequential).count();
-
-    cout << "Time measured (sequential): " 
-         << int (elapsed_time_sequential) 
-         << " milliseconds" << endl;
-
-    auto start_time_parallel = chrono::high_resolution_clock::now();
-    execute_parallel();
-    auto end_time_parallel = chrono::high_resolution_clock::now();
-    
-    float elapsed_time_parallel = chrono::duration<float, std::milli>
-                                            (end_time_parallel - start_time_parallel).count();
-    cout << "Time measured   (parallel): " 
-         << int (elapsed_time_parallel) 
-         << " milliseconds" << endl;
-
-    float speedup = (float)(elapsed_time_sequential / elapsed_time_parallel);
-
-    cout << "Checking for solution correctness..." << endl;
-    if (isSolutionCorrect())
+    for(int t : threads)
       {
-        cout << "Solution is correct" << endl;
-        cout << "Speedup: " << setprecision(3) << speedup << endl;
-      } 
-    else 
-      {
-        cout << "Solution is not correct\n";
+        cout << "Threads: " << t << endl;
+        for(int d : dimensions)
+          {
+            cout << "Matrix dimensions: [" << d << "][" << d << "]" << endl;
+            initialize_matrices(d, d);
+
+            auto start_time_sequential = chrono::high_resolution_clock::now();
+            execute_sequential(d, d);
+            auto end_time_sequential = chrono::high_resolution_clock::now();
+
+            float elapsed_time_sequential = chrono::duration<float, std::milli>
+                                                    (end_time_sequential - start_time_sequential).count();
+
+            cout << "Time measured (sequential): " 
+                << int (elapsed_time_sequential) 
+                << " milliseconds" << endl;
+
+            auto start_time_parallel = chrono::high_resolution_clock::now();
+            execute_parallel(d, d, t);
+            auto end_time_parallel = chrono::high_resolution_clock::now();
+            
+            float elapsed_time_parallel = chrono::duration<float, std::milli>
+                                                    (end_time_parallel - start_time_parallel).count();
+            cout << "Time measured   (parallel): " 
+                << int (elapsed_time_parallel) 
+                << " milliseconds" << endl;
+
+            float speedup = (float)(elapsed_time_sequential / elapsed_time_parallel);
+
+            cout << "Checking for solution correctness..." << endl;
+            if (isSolutionCorrect(d, d))
+              {
+                cout << "Solution is correct" << endl;
+                cout << "Speedup: " << setprecision(3) << speedup << endl;
+              } 
+            else 
+              {
+                cout << "Solution is not correct\n";
+              }
+          }
       }
 
     return 0;
@@ -79,15 +85,15 @@ int main(int argc, char *argv[])
  *
  * @retval void
  ******************************************************************************/
-void initialize_matrices()
+void initialize_matrices(int nr_rows, int nr_cols)
   {
-    sequential_matrix = new float *[NR_ROWS];
-    parallel_matrix   = new float *[NR_ROWS];
+    sequential_matrix = new float *[nr_rows];
+    parallel_matrix   = new float *[nr_rows];
 
-    for (int i = 0; i < NR_ROWS; i++)
+    for (int i = 0; i < nr_rows; i++)
       {
-        float *seq_row  = new float[NR_COLS];
-        float *par_row = new float[NR_COLS];
+        float *seq_row  = new float[nr_cols];
+        float *par_row = new float[nr_cols];
         sequential_matrix[i] = seq_row ;
         parallel_matrix  [i] = par_row;
 
@@ -95,7 +101,7 @@ void initialize_matrices()
         parallel_matrix   [i][0] = 150;
       }
 
-    for (int i = 0; i < NR_COLS; i++)
+    for (int i = 0; i < nr_cols; i++)
       {
         sequential_matrix [0][i] = 250;
         parallel_matrix   [0][i] = 250;
@@ -112,11 +118,11 @@ void initialize_matrices()
  *
  * @retval void
  ******************************************************************************/
-void execute_sequential()
+void execute_sequential(int nr_rows, int nr_cols)
   {
-    for (int i = 1; i < NR_ROWS; i++)
+    for (int i = 1; i < nr_rows; i++)
       {
-        for (int j = 1; j < NR_COLS; j++)
+        for (int j = 1; j < nr_cols; j++)
           {
             sequential_matrix[i][j] = (fabs(sin(sequential_matrix[i - 1][j - 1])) +
                                        fabs(sin(sequential_matrix[i][j - 1])) +
@@ -134,16 +140,16 @@ void execute_sequential()
  *
  * @retval void
  ******************************************************************************/
-void execute_parallel()
+void execute_parallel(int nr_rows, int nr_cols, int threads)
   {
     // iterate through diagionals
-    for (int i = 2; i < NR_ROWS + NR_COLS - 1; i++)
+    for (int i = 2; i < nr_rows + nr_cols - 1; i++)
       {
-  #pragma omp parallel for num_threads(THREADS)
+  #pragma omp parallel for num_threads(threads)
         for (int j = 1; j < i; j++)
           {
             int row = i - j;
-            if (row < NR_ROWS && j < NR_COLS)
+            if (row < nr_rows && j < nr_cols)
               {
                 parallel_matrix[row][j] = (fabs(sin(parallel_matrix[row    ][j - 1]))  +
                                            fabs(sin(parallel_matrix[row - 1][j - 1]))  +
@@ -163,12 +169,12 @@ void execute_parallel()
  * @retval true if the solution is correct
  * @retval false otherwise
  ******************************************************************************/
-bool isSolutionCorrect()
+bool isSolutionCorrect(int nr_rows, int nr_cols)
   {
     bool solutionCorrect = true;
-    for (int r = 1; r < NR_ROWS; r++)
+    for (int r = 1; r < nr_rows; r++)
       {
-        for (int c = 1; c < NR_COLS; c++)
+        for (int c = 1; c < nr_cols; c++)
           {
             if (sequential_matrix[r][c] != parallel_matrix[r][c])
               {
